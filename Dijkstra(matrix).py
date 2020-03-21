6ef63eb863e85b699c7da43adfa7a1019229e1ed
arunnair018 <arunnair018@gmail.com>
2020-03-21 22:15:32 +0530

    Dijkstra (adjacency matrix)

---
+++ Dijkstra(matrix).py
@@ -0,0 +1,47 @@
+
+maxint=9999
+class Graph(): 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] 
+                      for row in range(vertices)]
+    def printSolution(self, dist,src):
+        print ("Vertex Distance from Source node ",src)
+        for node in range(self.V):
+            print (node,"node -----> ",dist[node])
+    def minDistance(self, dist, sptSet):
+        min = maxint
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+        return min_index
+    def dijkstra(self, src):
+        dist = [maxint] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                        dist[v] = dist[u] + self.graph[u][v]
+        self.printSolution(dist,src)
+        
+x=int(input("No. of Nodes: "))
+g  = Graph(x)
+matrix=[]
+for i in range(x):
+    print('From node ',i)
+    matrix.append(list(map(int, input().split())))
+g.graph = matrix
+'''
+0 5 0 9 2 0
+5 0 2 0 0 0
+0 2 0 3 0 0
+9 0 3 0 0 2
+2 0 0 0 0 3 
+0 0 0 2 3 0
+ '''
+g.dijkstra(0)
+ 
